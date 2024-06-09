8bddf78527d72994c8f69830bedc4f7555032e63
pythonwithsean <pythonwithsean@gmail.com>
2024-06-09 19:14:04 +0100

    Server handler and init setup

--- Server/Server.go
+++ Server/Server.go
@@ -1,13 +1,30 @@
 package server
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Server struct {
 	addr string
 	port int64
 }
 
-func (s *Server) newSerber(addr string, port int64) *Server {
+type Delta struct {
+}
+
+func (s *Server) newServer(addr string, port int64) *Server {
 	return &Server{
 		addr: addr,
 		port: port,
 	}
+}
+
+func getDeltaHandler(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func (s *Server) Start() {
+	http.ListenAndServe(s.addr, nil)
+	fmt.Printf("Server Listening on %d", s.port)
 }
